a5637b3be049c1f1df0a27c3f437601c0958e084
slk <slk@slks-MacBook-Air.local>
2017-08-18 00:48:08 -0700

    Added autoload to app

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,27 @@
 from flask import Flask, jsonify, abort, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api, Resource
+import os
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 
-# Route Definitions
+# Auto generate Route Definitions
 # Import a module / component using its blueprint handler variable (mod_auth)
-from app.developer.controller import app_developer
+dl = next(os.walk('app'))[1]
 
-# Register blueprint(s)
-app.register_blueprint(app_developer)
+for i in dl:
+	temp_controller = 'app.' + i + '.controller'
+	temp_blueprint = 'app_' + i
+	temp_import = 'from ' + temp_controller + ' import ' + temp_blueprint
+	temp_blueprint_register = 'app.register_blueprint(' + temp_blueprint + ')'
+
+	# Import controllers
+	exec(temp_import)
+
+	# regiser blueprint
+	exec(temp_blueprint_register)
 
 # Create corresponding tables
 db.create_all()
