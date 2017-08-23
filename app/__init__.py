525f2d39c8621fdad8a60a44d5a18af3e0a55f3c
kravivar <kravivar@adobe.com>
2017-08-22 18:59:16 -0700

    Updated app to autoadd ui modules

--- app/__init__.py
+++ app/__init__.py
@@ -20,13 +20,27 @@ else:
 
 db = SQLAlchemy(app)
 
-# Import api blueprint
+# Import api blueprint under app/api
 from app.api import api_blueprint
-from app.ui.home.controller import home_blueprint
 
-# register 
+# register api blueprint
 app.register_blueprint(api_blueprint)
-app.register_blueprint(home_blueprint)
+
+# Auto register ui blueprint under app/ui
+
+dl = next(os.walk('app/ui'))[1]
+
+for i in dl:
+	temp_controller = 'app.ui.' + i + '.controller'
+	temp_blueprint = i + '_blueprint'
+	temp_import = 'from ' + temp_controller + ' import ' + temp_blueprint
+	temp_blueprint_register = 'app.register_blueprint(' + temp_blueprint + ')'
+
+	# Import ui controllers
+	exec(temp_import)
+
+	# regiser ui blueprint
+	exec(temp_blueprint_register)
 
 # Create corresponding tables
 db.create_all()
