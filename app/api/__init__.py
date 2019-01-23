c8fb893dd6e2b1d4bf3e0cd4d8459fd953e0f0bf
kripakaran ravivarman <kripakaranravivarman@kripakarans-MacBook-Pro.local>
2019-01-22 23:34:19 -0800

    Fixed a minor title issue for swagger

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -7,7 +7,7 @@ api_blueprint = Blueprint('api', __name__, url_prefix='/api')
 
 # Define restful api for blueprint
 api = Api(api_blueprint,
-	title='<a href="/">python flask full stack</a>',
+	title='python flask full stack',
 	version='1.0',
 	description='An API list for python stack',
 )
